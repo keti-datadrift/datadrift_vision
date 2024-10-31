a4434f4c9740896477694037cd85d64ff9e04e24
ybchoi@intellivix.ai <ybchoi@intellivix.ai>
2024-10-31 10:57:26 +0900

    project start

---
+++ dev_1year/vllm_model_server/client.py
@@ -0,0 +1,15 @@
+import requests
+import json
+from time import time
+
+data = { 
+	"instruct": "please describe this image"
+	}
+
+headers = {'Content-type': 'application/json'}
+t0=time()
+response = requests.post('http://locahost:1112/api/drift/v1/vlm_verify/', data=json.dumps(data), headers=headers)
+t1=time()
+print(t1-t0)
+print('****************************************************************')
+print('response.text=',response.text)
