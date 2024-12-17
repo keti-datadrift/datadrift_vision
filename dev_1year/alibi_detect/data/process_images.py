e4a30c7c8d4f4699ee32559baff9b640c1d4cd3c
snowa-intellivix <snowa@intellivix.ai>
2024-12-17 11:53:16 +0900

    Modularized code

---
+++ dev_1year/alibi_detect/data/process_images.py
@@ -0,0 +1,25 @@
+import os
+import random
+from image_to_list import image_to_list
+
+def process_images(orig_file_path, compare_file_path):
+    extensions = (".bmp", ".jpeg", ".jpg", ".png")
+
+    orig_image_paths = [os.path.join(orig_file_path,i) for i in os.listdir(orig_file_path) if os.path.splitext(i)[1] in extensions]
+    compare_image_paths = [os.path.join(compare_file_path,i) for i in os.listdir(compare_file_path) if os.path.splitext(i)[1] in extensions]
+
+    random.shuffle(orig_image_paths)
+
+    split_index = len(orig_image_paths) // 2
+    train_image_paths = orig_image_paths[:split_index]
+    test_image_paths = orig_image_paths[split_index:]
+
+    train_images = []
+    test_images = []
+    compare_images = []
+
+    image_to_list(train_image_paths, train_images)
+    image_to_list(test_image_paths, test_images)
+    image_to_list(compare_image_paths, compare_images)
+
+    return train_images, test_images, compare_images
---
+++ dev_1year/alibi_detect/data/transform_object.py
@@ -0,0 +1,30 @@
+import numpy as np
+from albumentations import (
+    Compose, GaussianBlur, MotionBlur, RandomBrightnessContrast, CoarseDropout
+)
+
+def transform_object(train_images, test_images):
+    gaussian_noise = GaussianBlur(blur_limit=7, p=1)  # 가우시안 노이즈
+    motion_blur = MotionBlur(blur_limit=7, p=1)  # 모션 블러
+    brightness = RandomBrightnessContrast(rightness_limit=0.2, p=1)  # 밝기 변형
+    pixelate = CoarseDropout(max_holes=8, max_height=8, max_width=8, fill_value=0, p=1)  # 픽셀화
+
+    # 변형을 각각 적용하고 결과를 서로 다른 변수에 저장
+    train_images_gaussian_noise = [gaussian_noise(image=image)["image"] for image in train_images]
+    train_images_motion_blur = [motion_blur(image=image)["image"] for image in train_images]
+    train_images_brightness = [brightness(image=image)["image"] for image in train_images]
+    train_images_pixelate = [pixelate(image=image)["image"] for image in train_images]
+
+    image_by_type = [train_images, test_images, train_images_gaussian_noise, train_images_motion_blur, train_images_brightness, train_images_pixelate, compare_images]
+    type_names = ["train_images", "test_images", "train_images_gaussian_noise", "train_images_motion_blur", "train_images_brightness", "train_images_pixelate", "compare_images"]
+
+    for idx, type in enumerate(image_by_type):
+        type = np.array(type)
+        type = np.stack(type)
+        image_by_type[idx] = type
+        print(f"{type_names[idx]} : {image_by_type[idx].shape}")
+
+    X_c = [image_by_type[i] for i in range(2, len(image_by_type))]
+    X_c_names = [type_names[i] for i in range(2, len(type_names))]
+
+    return image_by_type, X_c, X_c_names
