50d3870513c5aedf6afa8ae9ff1632da90ad8028
ybchoi@intellivix.ai <ybchoi@intellivix.ai>
2025-05-07 18:01:32 +0900

    remove folder dev_1year

--- dev_1year/alibi_detect/data/image_to_list.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-def image_to_list(paths, list):
-    for path in paths:
-        image = cv2.imread(path)
-        image = cv2.resize(image, (32, 32))
-        image = image.astype('float32')
-        list.append(image)
